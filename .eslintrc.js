597c070da466aa3fe0687618580a72474c4e2646
Mohamad Termanini <mohtermanini.job@gmail.com>
2022-03-18 12:33:00 +0200

    Bundle js and sass files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    "no-plusplus": ["off"],
-    "linebreak-style": ["error", "windows"],
-    quotes: ["error", "double"],
-    indent: ["error", 4],
-    "no-param-reassign": ["off"]
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        "no-plusplus": ["off"],
+        "linebreak-style": ["error", "windows"],
+        quotes: ["error", "double"],
+        indent: ["error", 4],
+        "no-param-reassign": ["off"],
+        "no-use-before-define": ["off"],
+        "no-new" : ["off"],
+        "no-labels": ["off"],
+    },
 };
