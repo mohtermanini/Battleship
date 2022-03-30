a8a9da4dc4e46265455aa2d756d6b34c87cfa4fe
Mohamad Termanini <mohtermanini.job@gmail.com>
2022-03-31 00:48:20 +0300

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,16 +10,16 @@ module.exports = {
     },
     module: {
         rules: [
-            // {
-            //     test: /.js$/,
-            //     exclude: /(node_modules|bower_components)/,
-            //     use: {
-            //         loader: "babel-loader",
-            //         options: {
-            //             presets: ["@babel/preset-env"],
-            //         },
-            //     },
-            // },
+            {
+                test: /.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
             {
                 test: /\.handlebars$/, loader: "handlebars-loader",
             },
