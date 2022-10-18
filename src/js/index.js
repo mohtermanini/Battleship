031be52de1a8bd9846dd4f6fd282bc47b85b9623
Mohamad Termanini <mohtermanini.job@gmail.com>
2022-10-18 23:28:27 +0300

    Load scss using webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import GameController from "./Game/game-controller";
 import GameEventHandler from "./Game/game-event-handler";
+import "../sass/styles.scss";
 
 const gamesContainer = document.getElementById("games");
 GameController.generateGame(gamesContainer, 1);
