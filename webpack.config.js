684945cf3b1e46f65d982d167037d2b8c982c196
Mohamad Termanini <mohtermanini.job@gmail.com>
2022-11-03 05:44:56 +0300

    Autoload favicon

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "index.html"
+            template: "index.html",
+            favicon: "./favicon.png"
         })
     ]
 };
